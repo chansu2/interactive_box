c777d41129580c0ad1aeba67016d1e0258369d9e
eddy <kcs7374@gmail.com>
2024-01-10 11:53:39 +0900

    chagne didUpdateWidget at InteractiveState

--- lib/src/interactive_box.dart
+++ lib/src/interactive_box.dart
@@ -191,9 +191,14 @@ class InteractiveBoxState extends State<InteractiveBox> {
   void didUpdateWidget(InteractiveBox oldWidget) {
     bool didUpdated = false;
 
-    if (oldWidget.initialShowActionIcons != widget.initialShowActionIcons) {
-      _showItems = widget.initialShowActionIcons;
-      didUpdated = true;
+    // if (oldWidget.initialShowActionIcons != widget.initialShowActionIcons) {
+    //   _showItems = widget.initialShowActionIcons;
+    //   didUpdated = true;
+    // }
+    if (oldWidget.showItems != widget.showItems) {
+      setState(() {
+        _showItems = widget.showItems; // 재구축 시점에 사용
+      });
     }
     if (oldWidget.initialSize.width != widget.initialSize.width) {
       _width = widget.initialSize.width;
